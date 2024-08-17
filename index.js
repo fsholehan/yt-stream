844968f2e564113c831164fb8d24625e6218f7f3
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2024-08-17 08:53:12 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import cors from "cors";
+import fetch from "node-fetch";
+import { Innertube } from "youtubei.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+const visitor_data = process.env.VISITOR_DATA;
+const po_token = process.env.PO_TOKEN;
+
+app.get("/stream/:videoId", async (req, res) => {
+  const youtube = await Innertube.create({ visitor_data, po_token });
+  try {
+    const videoId = req.params.videoId;
+
+    const info = await youtube.getBasicInfo(videoId);
+    const format = info.chooseFormat({ type: "audio", quality: "best" });
+
+    if (!format) {
+      return res.status(404).send("Audio format not found.");
+    }
+
+    let url = format?.decipher(youtube.session.player);
+
+    if (!url) {
+      return res.status(500).send("Failed to get the audio stream URL.");
+    }
+
+    // Streaming data ke client
+    fetch(url.toString())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch audio stream.");
+        }
+        res.setHeader("Content-Type", "audio/mpeg");
+        response.body.pipe(res);
+      })
+      .catch((error) => {
+        res.status(500).send(error.message);
+      });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
